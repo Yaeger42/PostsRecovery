9dc6192746605686131650898e18cb3bb46b6db5
Diego Duarte <projectdcd@gmail.com>
2020-12-31 18:12:06 -0600

    MYSQL connection in local Docker working, only missing to actually make it work in master and everything on docker

--- recover/settings.py
+++ recover/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'recover.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'djangouser',
+        'PASSWORD': env("MYSQL_PASSWORD"),
+        'HOST' : '127.0.0.1',
+        'PORT': 6603
     }
 }
 
