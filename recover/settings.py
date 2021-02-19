e29d419207e7e2d3b7e25a56e66be0f04f18d40f
Diego Duarte <projectdcd@gmail.com>
2021-02-19 17:07:52 -0600

    deleted unnecesary imports

--- recover/settings.py
+++ recover/settings.py
@@ -13,12 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import allauth
-# import environ
-
-# # Environment configuration
-# env = environ.Env()
-# environ.Env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
